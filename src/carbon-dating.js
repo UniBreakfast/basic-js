2c66caf7dc966357bf05d2655443a52404752e83
unibreakfast <unibreakfast1@gmail.com>
2020-02-19 17:12:18 +0200

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (!sampleActivity || typeof sampleActivity != 'string') return false
+
+  sampleActivity = parseFloat(sampleActivity)
+  if (sampleActivity > MODERN_ACTIVITY ||
+      sampleActivity < 0 ||
+     !sampleActivity) return false
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity) *
+    HALF_LIFE_PERIOD / 0.693);
 };
