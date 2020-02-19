c229d013349390b9feca81e2a7440de0a06ec1b1
unibreakfast <unibreakfast1@gmail.com>
2020-02-19 17:48:42 +0200

    feat: what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,16 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+
+  if (date === undefined) return 'Unable to determine the time of year!'
+
+  if (!(date instanceof Date) ||
+      String(new Date(date.getTime())) != String(date))
+        throw new Error('value provided is not a date')
+
+  const month = date.getMonth()
+  switch (true) {
+    case month < 2 || month > 10: return 'winter'
+    case month < 5: return 'spring'
+    case month < 8: return 'summer'
+    default: return 'autumn'
+  }
 };
