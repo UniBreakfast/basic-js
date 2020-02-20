6da8a61aeeae4e40dbca4d7968a79b0e4ab314ce
unibreakfast <unibreakfast1@gmail.com>
2020-02-20 08:18:19 +0200

    feat: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,16 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('arr is not a valid Array')
+
+  return arr.map((item, i) => {
+    if (['--discard-next', '--discard-prev', '--double-next','--double-prev']
+      .includes(item)) return []
+
+    const part = [item]
+    let iNext = i+1
+    while (arr[iNext] == '--discard-next') iNext+=2
+    if (arr[i-1]=='--discard-next' || arr[iNext]=='--discard-prev') part.pop()
+    if (arr[i-1]=='--double-next') part.push(item)
+    if (arr[i+1]=='--double-prev') part.push(item)
+    return part
+  }).flat()
 };
